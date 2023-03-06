769b27474333a4dde4eab1e40a8f017d8355bd39
mberndt92 <maximilian_berndt@icloud.com>
2023-03-06 20:34:08 +0900

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Maximilian Berndt on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
